f8bffafc259cb89a29d94588def182f8b37bd572
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-07-16 03:04:18 +0600

    first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navber from "../Navber/Navber";
+
+const Main = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Components/Navber/Navber.jsx
@@ -0,0 +1,18 @@
+
+const Navber = () => {
+    return (
+        <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none justify-center ">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Navber;
